a7239e7e76ce0add848fa000181d69c7eddf0cc3
Right-Click-Code <76565060+Right-Click-Code@users.noreply.github.com>
2020-12-24 19:36:40 +0330

    Add files via upload

---
+++ game.py
@@ -0,0 +1,9 @@
+# Right Click
+car = input("Enter your Love Car: ")
+color = input("Enter your like color: ")
+actor = input("Enter your Feavrity Actor(s): ")
+
+print("\n\n")
+print("You Like buy " + car)
+print("You LOVE the Color " + color)
+print("You Fave Actor is: " + actor)
